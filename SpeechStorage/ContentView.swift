7c6290922c192ac02e02d716eb172265a3ddda28
sakurakotubaki <gitonly543@gmail.com>
2025-01-03 18:00:53 +0900

    Initial Commit

---
+++ SpeechStorage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeechStorage
+//
+//  Created by 橋本純一 on 2025/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeechStorage/SpeechStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechStorageApp.swift
+//  SpeechStorage
+//
+//  Created by 橋本純一 on 2025/01/03.
+//
+
+import SwiftUI
+
+@main
+struct SpeechStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
